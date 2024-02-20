da5bd7d78447ac9426d60274ab5302f552e67c30
Umair2008ali <160146974+Umair2008ali@users.noreply.github.com>
2024-02-20 15:24:36 -0800

    Add files via upload

---
+++ J.S task/app.js
@@ -0,0 +1,84 @@
+//   part 1 
+// // Question one
+
+ alert("please!Enter a valid password")
+
+// // Question Two
+ alert("Welcome to J.S land... \n Happy Coding!")
+
+// // Question Three
+ alert("Welcome to JS land..")
+
+// // Question Four
+ alert("Happy coding.. \n prevent this page from creating additonal dialouges")
+
+// // Question Five
+ alert("Hello... I can run JS my web Browser's console")
+
+
+// // part 2
+// // question 4
+ alert("John Doe")
+
+// // question 5
+ alert("I am 15 years old")
+
+// // question 6
+alert("Certified Mobile Application Development")
+
+// // question 7
+ alert("PIZZA \n PIZZ \n PIZ \n PI \n P \n")
+
+// // question 8
+ alert("My EMAIL ADREES is umairftffd@gmail.com")
+
+// // question 9
+ alert("I am trying to learn a book a smarter way to learn JS")
+
+// // question 10
+document.write("<center>Yeah I can write <big><b>HTML</b></big> content in <big><b>JAVA SCRIPT</b></big></center>")
+
+
+// question11
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+
+
+
+// part 3
+// question1
+alert("I am 15 years old")
+
+// question2
+alert("You have visited this site 14 times")
+
+
+// question3
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write(" <center><b><i>My birthyear is 2008</i></b></center>")
+document.write("<br>")
+document.write("<center>Data type of my declared variable is number</center>")
+
+// question4
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("<center><b>JohnDoe</b> ordered <b>5 T-shirt(s)</b> on XYZ Clothing store...</center>")
+
+
+
+// part4
+// question1
+alert("I. First Name:-Umair \n II. Last Name:-Ali \n III. Email:-dummyemail1@gmail.com \n IV. Phone Number:- +92 334 9797655 \n V. Password:-********")
+
+// question2
+alert ( "ALERT! You are learning JavaScript!,")
+
+// question 3;
+window.confirm("sometext");
+
+// question 4
+
